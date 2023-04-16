70e0c9ef13b64d66018ed5c4bad7f5f133bce570
Klaus-Eduard Runnel <klaus-eduard.runnel@nortal.com>
2023-04-16 20:26:09 +0300

    Add error handling, parse args in separate method
    All suggested by chatGPT again

--- to_text.py
+++ to_text.py
@@ -4,7 +4,22 @@ import argparse
 from bs4 import BeautifulSoup
 import html2text
 
-def main(input_file, output_file):
+
+def parse_arguments() -> argparse.Namespace:
+    """
+    Parse command-line arguments.
+
+    Returns:
+        argparse.Namespace: Parsed command-line arguments.
+    """
+    parser = argparse.ArgumentParser(description='Convert Confluence JSON to plain text.')
+    parser.add_argument('input_file', type=str, help='Path to the input JSON file.')
+    parser.add_argument('output_file', type=str, help='Path to the output plain text file.')
+
+    return parser.parse_args()
+
+
+def to_text(input_file:str, output_file:str) -> None:
     """
     Convert a Confluence JSON file to plain text, preserving preformatted text.
 
@@ -12,9 +27,13 @@ def main(input_file, output_file):
         input_file (str): Path to the input JSON file.
         output_file (str): Path to the output plain text file.
     """
-    with open(input_file, 'r') as f:
-        data = json.load(f)
-        html_content = data['body']['storage']['value']
+    try:
+        with open(input_file, 'r') as f:
+            data = json.load(f)
+            html_content = data['body']['storage']['value']
+    except (FileNotFoundError, IOError) as e:
+        print(f"Error reading input file: {e}")
+        sys.exit(1)
 
     soup = BeautifulSoup(html_content, 'html.parser')
 
@@ -29,14 +48,17 @@ def main(input_file, output_file):
     text_maker.ignore_images = True
     plain_text = text_maker.handle(str(soup))
 
-    with open(output_file, 'w') as f:
-        f.write(plain_text)
+    try:
+        with open(output_file, 'w') as f:
+            f.write(plain_text)
+    except (FileNotFoundError, IOError) as e:
+        print(f"Error writing output file: {e}")
+        sys.exit(1)
+
+def main() -> None:
+    args = parse_arguments()
+    to_text(args.input_file, args.output_file)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Convert Confluence JSON to plain text.')
-    parser.add_argument('input_file', help='Path to the input JSON file.')
-    parser.add_argument('output_file', help='Path to the output plain text file.')
-
-    args = parser.parse_args()
-    
-    main(args.input_file, args.output_file)
+    main()
